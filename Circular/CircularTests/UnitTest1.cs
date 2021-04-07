d6902eb2d8cba5f06a4bb59c1643c54dcf4c074c
BogdanYatsiv <53473630+BogdanYatsiv@users.noreply.github.com>
2021-04-07 13:31:33 +0300

    Restructuraize

--- Circular/CircularTests/UnitTest1.cs
+++ Circular/CircularTests/UnitTest1.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 
-namespace TestProject
+namespace CircularTests
 {
     public class UnitTest1
     {
---
+++ Circular/DAL/Identity/Project.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Identity
+{
+    [Table("Project")]
+    class Project
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+        [Required]
+        public string githubLink { get; set; }
+
+        [Required]
+        public DateTime dateTime { get; set; }
+
+        public List<User> User { get; set; }
+    }
+}
