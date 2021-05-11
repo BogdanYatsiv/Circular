02c137a4e74816b02586f177479d469c62f4b485
BogdanYatsiv <53473630+BogdanYatsiv@users.noreply.github.com>
2021-05-11 17:21:55 +0300

    Add api request

---
+++ Circular/Circular/Controllers/RequestController.cs
@@ -0,0 +1,34 @@
+﻿using Circular.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Circular.Controllers
+{
+    public class RequestController : Controller
+    {
+        // GET: RequestController
+        [HttpGet]
+        public async Task<IActionResult> Commits(string ApiUrl = "https://api.github.com/repos/BogdanYatsiv/Circular/commits")
+        {
+            string RequestResult;
+            WebRequest request = WebRequest.Create(ApiUrl);
+            WebResponse response = await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    RequestResult = reader.ReadToEnd();
+                }
+            }
+            response.Close();
+            ViewData["RequestResult"] = RequestResult;
+            return View();
+        }
+    }
+}
