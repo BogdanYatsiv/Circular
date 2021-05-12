061a1afe7ee05bcb4a00376aec8b1ae4b2739ec0
BogdanYatsiv <53473630+BogdanYatsiv@users.noreply.github.com>
2021-05-12 13:33:00 +0300

    Add formatting for url

--- Circular/Circular/Controllers/RequestController.cs
+++ Circular/Circular/Controllers/RequestController.cs
@@ -16,15 +16,17 @@ namespace Circular.Controllers
 {
     public class RequestController : Controller
     {
-        // GET: RequestController
         [HttpGet]
-        public async Task<IActionResult> Commits(string ApiUrl = "https://api.github.com/repos/BogdanYatsiv/Circular/commits")
+        public async Task<IActionResult> Commits(string RepoUrl = "https://github.com/BogdanYatsiv/Circular")
         {
             string JsonRequestResult;
             List<CommitResponse> commits = new List<CommitResponse>();
+            var toFormat = RepoUrl.Split("/");
+            string apiUrl = string.Format("https://api.github.com/repos/{0}/{1}/commits", toFormat[3], toFormat[4]);
+
 
             //Request body
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(ApiUrl);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
             //UserAgent required for GitHub API
             request.UserAgent = "Google Chrome 40 (Win 8.1 x64): Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36";
             WebResponse response = await request.GetResponseAsync();
@@ -37,8 +39,7 @@ namespace Circular.Controllers
             }
             response.Close();
 
-            //JsonRequestResult is a array of json nums
-            //TO DO: розпарсити джейсон і передати на сторінку нормальні дані
+            //JsonRequestResult is an array of json nums
             JArray jR = JArray.Parse(JsonRequestResult);
             
             foreach (JObject value in jR)
