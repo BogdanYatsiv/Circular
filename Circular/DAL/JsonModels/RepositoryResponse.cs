898fec3d7fe7ac1c54d3b3af9e48d1d30137c59b
BogdanYatsiv <53473630+BogdanYatsiv@users.noreply.github.com>
2021-05-16 17:37:14 +0300

    add repositoryResponse model

---
+++ Circular/DAL/JsonModels/RepositoryResponse.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.JsonModels
+{
+    class RepositoryResponse
+    {
+        public string name { get; set; }
+        public Owner owner { get; set; }
+
+        [JsonProperty("html_url")]
+        public string url { get; set; }
+
+        public string language { get; set; }
+    }
+
+    class Owner
+    {
+        [JsonProperty("login")]
+        public string name { get; set; }
+    }
+}
