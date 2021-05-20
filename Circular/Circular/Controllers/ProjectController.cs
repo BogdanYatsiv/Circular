6ff3c2e99389d9401ccc6efd4c0c37a4a121d21e
BogdanYatsiv <53473630+BogdanYatsiv@users.noreply.github.com>
2021-05-20 12:26:38 +0300

    Fix project creation

--- Circular/Circular/Controllers/ProjectController.cs
+++ Circular/Circular/Controllers/ProjectController.cs
@@ -6,19 +6,22 @@ using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.IO;
 using Circular.Models.JsonModels;
+using Circular.Models.ViewModels;
 
 namespace Circular.Controllers
 {
     public class ProjectController : Controller
     {
+        [HttpGet]
         public async Task<IActionResult> Project(string GithubLink)
         {
             //TO DO: заносити проект в базу даних
             string JsonRequestResult;
             ProjectResponse projectResponse = new ProjectResponse();
-            var toFormat = GithubLink.Split("/");
+            //TO DO: правильно відформатувати посилання
+            //var toFormat = GithubLink.Split("/");
             string apiUrl = string.Format("https://api.github.com/repos/{0}/{1}",
-                toFormat[toFormat.Count() - 1], toFormat[toFormat.Count() - 2]);
+                "BogdanYatsiv", "Circular");
 
             //Request body
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
@@ -34,20 +37,36 @@ namespace Circular.Controllers
             }
             response.Close();
 
-            //JsonRequestResult is an array of json nums
-            JArray jR = JArray.Parse(JsonRequestResult);
+            //TO DO: спарсити як 1 об'єкт а не список
 
-            foreach (JObject value in jR)
-            {
-                projectResponse = JsonConvert.DeserializeObject<ProjectResponse>(value.ToString());
-            }
+            //JsonRequestResult is an array of json nums
+            //JArray jR = JArray.Parse(JsonRequestResult);
+
+            //foreach (JObject value in jR)
+            //{
+            //    projectResponse = JsonConvert.DeserializeObject<ProjectResponse>(value.ToString());
+            //}
+            JObject jO = JObject.Parse(JsonRequestResult);
+            projectResponse = JsonConvert.DeserializeObject<ProjectResponse>(jO.ToString());
 
             return View(projectResponse);
         }
 
-        public IActionResult CreateProject(string GithubLink)
+        [HttpGet]
+        public IActionResult CreateProject()
         {
             //return RedirectToAction("Project", "Project", new { GithubLink });
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProject(CreateProjectModel model)
+        {
+            if(model.GithubLink.Length >= 0)
+            {
+                return RedirectToAction("Project", "Project", new { model.GithubLink });
+            }
+
             return View();
         }
     }
